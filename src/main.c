116f23080a93c0f11b1f3292dacf2be5ee0dad7f
csamga <gabin.mascioli@gmail.com>
2023-03-09 09:40:22 +0100

    Added basic window creation

--- src/main.c
+++ src/main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
 	printf("hi\n");
 
-	return 0;
+	GLFWwindow *window;
+
+	if (!glfwInit()) {
+		return EXIT_FAILURE;
+	}
+
+	window = glfwCreateWindow(960, 540, "Test", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return EXIT_SUCCESS;
 }
